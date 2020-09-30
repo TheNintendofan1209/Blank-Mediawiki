22ed5cc9c1648651bed189bac74afdd6d7c04ba1
TheNintendofan1209 <mike.nintendofan1209@gmail.com>
2020-09-29 21:45:08 -0500

    Add files via upload

--- includes/SkinBlank.php
+++ includes/SkinBlank.php
@@ -4,36 +4,18 @@
  *
  * @ingroup Skins
  */
-class SkinBlank extends SkinTemplate {
-	public $skinname = 'blank',
-		$stylename = 'Blank',
-		$template = 'BlankTemplate';
+class SkinBlank extends SkinMustache {
+	public $template = 'BlankTemplate';
 
 	/**
-	 * Add CSS via ResourceLoader
-	 *
-	 * @param $out OutputPage
+	 * @inheritDoc
 	 */
-	public function initPage( OutputPage $out ) {
-		$out->addMeta( 'viewport',
-			'width=device-width, initial-scale=1.0, ' .
-			'user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0'
-		);
-
-		$out->addModuleStyles( [
-			'mediawiki.skinning.interface',
-			'mediawiki.skinning.content.externallinks',
-			'skins.blank'
-		] );
-		$out->addModules( [
-			'skins.blank.js'
-		] );
-	}
-
-	/**
-	 * @param $out OutputPage
-	 */
-	function setupSkinUserCss( OutputPage $out ) {
-		parent::setupSkinUserCss( $out );
+	public function getTemplateData() {
+		$data = parent::getTemplateData();
+		$tpl = $this->prepareQuickTemplate();
+		$tplData = $tpl->execute();
+		return $data + $tplData + [
+			'blank-main-page-url' => self::makeMainPageUrl(),
+		];
 	}
 }
